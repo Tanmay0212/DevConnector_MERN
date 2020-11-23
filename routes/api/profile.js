d46029d0d19be6922e2a8978cbca3143209d1eec
Tanmay Rajesh Bhardwaj <tbhardwa@tibco.com>
2020-11-23 23:19:09 +0530

    Like and unlike a post

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
 const { json } = require('express');
+const request = require('request');
+const config = require('config');
 
 //@route GET api/profile/me
 //@desc get current users profile
@@ -316,4 +318,35 @@ router.delete('/education/:exp_id', auth, async (req, res) => {
     res.status(500).send('Server error!');
   }
 });
+
+//@route GET api/profile/github/:username
+//@desc  GET user repos from github
+//@access Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found!' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!');
+  }
+});
+
 module.exports = router;
